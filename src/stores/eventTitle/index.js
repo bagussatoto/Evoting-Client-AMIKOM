ddc53e3f553eb43d8aef9009de95922b3abd84e7
BAGUS BUDI SATOTO <87259393+bagussatoto@users.noreply.github.com>
2022-08-15 03:00:15 +0700

    commit

--- src/stores/eventTitle/index.js
+++ src/stores/eventTitle/index.js
@@ -4,7 +4,7 @@ import { defineStore } from 'pinia'
 export const useEventTitle = defineStore('eventTitle', {
 	state: () => {
 		return {
-			eventTitle: "Electronic Vote System Pemuda RT 01"
+			eventTitle: "Electronic Vote System Universitas Amikom Yogyakarta"
 		}
 	},
 	actions: {
